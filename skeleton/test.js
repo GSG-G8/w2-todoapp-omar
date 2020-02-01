bd63fb8691b9408a9197d2838872d54ec5d800d5
[omar dahdouh] <[omar.m.dahdouh@gmail.com]>
2020-02-01 11:46:25 +0200

    installing jest relates #1

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,7 +1,5 @@
-var test = require('tape');
-var logic = require('./logic');
+const todoFunctions = require('./logic.js');
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
+test('Example test', function () {
+  expect(true).toBe(true);
 });
